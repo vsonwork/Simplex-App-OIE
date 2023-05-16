7c75b1e1c0b03853afb16e09c94fd49283a885a7
VSON <81970770+vsonwork@users.noreply.github.com>
2023-05-16 08:47:41 +0700

    Update 1_📺_Tutorial.py

--- 1_📺_Tutorial.py
+++ 1_📺_Tutorial.py
@@ -8,7 +8,24 @@ st.set_page_config(
 	layout="wide"
 )
 
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] > .main {{
+background-image: url("https://raw.githubusercontent.com/vsonwork/Simplex-App-OIE/main/img/background_main.jpg?token=GHSAT0AAAAAACBEEOWHVKVZSZE5WPCU7ZVCZDC4CBQ");
+background-size: 150%;
+background-position: top left;
+background-repeat: repeat;
+background-attachment: local;
+}}
 
+[data-testid="stSidebar"] > div:first-child {{
+background-image: url("https://raw.githubusercontent.com/vsonwork/Simplex-App-OIE/main/img/background_sida.webp?token=GHSAT0AAAAAACBEEOWGKNFY6SERGIVWB3W6ZDC4CIA");
+background-position: center; 
+background-repeat: no-repeat;
+background-attachment: fixed;
+}}
+</style>
+"""
 
 _, col_video, _ = st.columns([1, 8, 1], gap="large")
 video_file = open("powerBI.mp4", 'rb')
